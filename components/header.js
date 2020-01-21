14b739090b2c93de21852af43bd7f2be82dbe616
Shu Ding <g@shud.in>
2020-01-21 21:12:55 +0800

    change repo url

--- components/header.js
+++ components/header.js
@@ -8,7 +8,7 @@ export default function Header () {
   return <CMS endpoint="/wp-json?_fields=name,description">{({ name, description }) =>
     <div className={styles.header}>
       <h1><Link href="/"><a>{name}</a></Link></h1>
-      <a href="https://github.com/zeit/next-cms" target="_blank" className={styles.tip}>What is this?</a>
+      <a href="https://github.com/quietshu/next-cms" target="_blank" className={styles.tip}>What is this?</a>
       <i>{description}</i>
       <Head><title>{name}</title></Head>
     </div>
