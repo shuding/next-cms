1dac8a09f4bd474aa620d1169035006092848550
sinchang <sinchangwen@gmail.com>
2020-05-11 10:06:08 +0800

    Update branding in footer

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,27 @@
 import styles from '../styles/main.module.css'
 
-export default function Footer () {
-  return <footer className={styles.footer}>
-    <a href="https://zeit.co" target="_blank" rel="noopener noreferrer" aria-label="Made by ZEIT">
-      <svg xmlns="http://www.w3.org/2000/svg" width="230" height="24" viewBox="0 0 230 46">
-        <defs>
-          <linearGradient id="a" x1="114.721%" x2="39.54%" y1="181.283%" y2="100%">
-            <stop offset="0%" stopColor="#FFF"/>
-            <stop offset="100%"/>
-          </linearGradient>
-        </defs>
-        <g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" transform="translate(-235 -177) translate(235 177)">
-          <path fill="url(#a)" d="M25.49162 0L50.98324 45.226415 0 45.226415z"/>
-          <path fill="#333" d="M85.75 34h20.45v-3.55H90.85l15.1-21.55V5.8H86v3.55h14.85L85.75 30.9V34zm41.85 0h18.35v-3.55h-14.2V21.4h12.35v-3.55h-12.35v-8.5h14.2V5.8H127.6V34zm40.45 0h17.9v-3.55h-6.85V9.35h6.85V5.8h-17.9v3.55h6.9v21.1h-6.9V34zm47.35 0h4.15V9.35h9.6V5.8H205.9v3.55h9.5V34z"/>
-        </g>
-      </svg>
-    </a>
-  </footer>
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href='https://vercel.com'
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label='Made by Vercel'
+      >
+        <svg
+          width='283'
+          height='64'
+          viewBox='0 0 283 64'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z'
+            fill='#000'
+          />
+        </svg>
+      </a>
+    </footer>
+  )
 }
