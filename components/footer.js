d1819df194ded6d1d5d64d085d8e2292271e69cb
Jeff Wen <sinchangwen@gmail.com>
2020-05-11 12:31:21 +0800

    Update components/footer.js
    Co-authored-by: Shu Ding <ds303077135@gmail.com>

--- components/footer.js
+++ components/footer.js
@@ -10,8 +10,7 @@ export default function Footer() {
         aria-label='Made by Vercel'
       >
         <svg
-          width='283'
-          height='64'
+          width='150'
           viewBox='0 0 283 64'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
